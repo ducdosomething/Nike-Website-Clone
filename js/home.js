cccff3647426e333290da664a3f4cfed0220e4a8
ducdosomething <duclehoang211201@gmail.com>
2024-02-23 17:09:56 +0700

    Upload home page

---
+++ js/home.js
@@ -0,0 +1,8 @@
+function displayProduct(){
+    let productItem = document.getElementById("pro");
+    for(let i = 0; i < productItem; i++){
+        document.write("productItem");
+    }
+}
+
+displayProduct();
